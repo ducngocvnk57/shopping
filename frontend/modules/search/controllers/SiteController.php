b475c1710c540b5b6c12951d87fa1d350cc5d4f5
HoangVu <kingrain94@gmail.com>
2016-11-01 19:52:52 +0700

    search keyword

--- frontend/modules/search/controllers/SiteController.php
+++ frontend/modules/search/controllers/SiteController.php
@@ -1,40 +1,43 @@
 <?php
 namespace frontend\modules\search\controllers;
 
+use frontend\modules\search\models\Product;
 use Yii;
+use yii\data\Pagination;
 use yii\web\Controller;
-use yii\filters\VerbFilter;
-use yii\filters\AccessControl;
-use common\models\LoginForm;
-use common\models\User;
+use frontend\modules\search\models\SearchForm;
 /**
  * Site controller
  */
 class SiteController extends Controller
 {
-  /**
-   * @inheritdoc
-   */
+    public $enableCsrfValidation = false;
 
-  /**
-   * @inheritdoc
-   */
-  public function actions()
-  {
-    return [
-        'error' => [
-            'class' => 'yii\web\ErrorAction',
-        ],
-    ];
-  }
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
 
-  /**
-   * Displays homepage.
-   *
-   * @return string
-   */
-  public function actionIndex()
-  {
-    return $this->render('index');
-  }
+    public function actionIndex()
+    {
+        if (isset($_POST['searchKeyword'])) {
+            $text = $_POST['searchKeyword'];
+        } else {
+            $text = "";
+        }
+        $query = Product::find()->where(['like', 'title', $text]);
+        $page = new Pagination(['defaultPageSize' => 8, 'totalCount' => $query->count()]);
+        $model = $query->offset($page->offset)
+            ->limit($page->limit)
+            ->all();
+
+        return $this->render('index', [
+            'model' => $model,
+            'page' => $page
+        ]);
+    }
 }
--- frontend/modules/search/views/site/index.php
+++ frontend/modules/search/views/site/index.php
@@ -1,1 +1,49 @@
-<h1>Order module</h1>
+<?php
+?>
+<div class="row">
+    <div class="col-md-3">
+        <form action="?r=search/site/index" method="post">
+            <div class="input-group">
+                <input class="form-control" placeholder="Search" name="searchKeyword" type="text">
+                <div class="input-group-btn">
+                    <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class="col-md-9">
+        <?php
+        if ($model != null) {
+            if (sizeof($model) > 0 ) {
+                foreach ($model as $item) {
+                    ?>
+                    <div class="col-sm-3 col-lg-3 col-md-3">
+                        <div class="thumbnail" style="height: 500px;overflow: hidden">
+                            <figure class="images" ><img  src="<?=$item->image;?>" alt=""></figure>
+                            <div class="caption">
+                                <h4 class="pull-right">Dong</h4>
+                                <h4><a href="?r=order/site/detail&id=<?=$item->id?>"><?=$item->title?></a>
+                                </h4>
+                                <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+                            </div>
+                            <div class="ratings">
+                                <p class="pull-right"><?=$item->viewed?> veviewed</p>
+                                <p>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                    <span class="glyphicon glyphicon-star"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                <?php
+                }
+            } else {
+                echo "<div class='container'><h4>Không tìm thấy kết quả nào hợp lệ</h4></div>";
+            }
+        }
+        ?>
+    </div>
+</div>
