51e1df3cf52bbebb68891541eeeeacc1d06b4d2f
Duc Ngoc <ducngocvnk57@gmail.com>
2016-12-07 19:36:39 +0700

    user

--- frontend/modules/search/controllers/SearchTitle.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace frontend\modules\search\controllers;
-
-class SearchTitle implements SearchInterface{
-  public static function search($value="",$product=null){
-      return $query = $product->andwhere(['like', 'title', $value]);
-  }
-}
-?>
